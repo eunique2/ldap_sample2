064f23c099ce9b2850b18edefdf98de771cac139
Doopyo Hong 홍두표 (Andre) <artisthong@dongwha.com>
2018-02-27 11:09:57 +0900

    make compress file

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const ldap = require('ldapjs');
-const baseDN = 'DC=dongwhadev,DC=com',
-  baseAdminID = 'ADLdap',
-  baseAdminPW = '1AopenAD1!';
+var ldap = require('ldapjs');
+var baseDN = 'DC=dongwhadev,DC=com';
+var baseAdminID = 'ADLdap';
+var baseAdminPW = '1AopenAD1!';
 
-const client = ldap.createClient({
+var client = ldap.createClient({
   url: 'ldap://172.16.0.140:389',
   baseDN: baseDN,
   bindDN: baseAdminID,
@@ -11,28 +11,19 @@ const client = ldap.createClient({
 });
 
 client.on('connect', function() {
-  console.log('possible connection..');
-  // client.exop('1.3.6.1.4.1.4203.1.11.3', function(err, value, res) {
-  //   console.log('whois: ' + res);
-  //   console.log('whois: ' + value);
-  // });
-  // client.bind('cn='+baseAdminID,baseAdminPW,function(err){
-  //   console.log(err);
-  // })
+  console.log('LDAP is connected..');
 
-  const searchOptions = {
+  var searchOptions = {
     scope: "sub",
     filter: '(sAMAccountName=d4001367)'
   };
   client.search(baseDN, searchOptions, function(err, res) {
     res.on('searchEntry', function(entry) {
-      console.log('entry: ' + JSON.stringify(entry.object));
-
-      client.bind(entry.object.dn, 'P@4001367!!', function(err){
-        if(err==null){
+      console.log('entry: ' + JSON.stringify(entry.object.dn));
+      client.bind(entry.object.dn, 'P@4001367!!', function(err) {
+        if (err == null) {
           console.log("it's true");
-        }
-        else {
+        } else {
           console.log("it's false");
         }
       });
@@ -47,9 +38,8 @@ client.on('connect', function() {
     res.on('end', function(result) {
       console.log('status: ' + result.status);
       client.unbind(function(err) {
-        console.log(err);
+        console.log('LDAP is disconnected..');
       });
-
     });
   });
 });
